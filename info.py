8c8a08c7cceabcc6efedb9f28dd1d8a6fd0534c7
Anlimali <93004643+Anlimali@users.noreply.github.com>
2025-11-06 11:13:12 +0530

    Update SHORTLINK_URL and SHORTLINK_API values

--- info.py
+++ info.py
@@ -95,8 +95,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/+Ws7VRUlD3CdkNzA1')  
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = is_enabled(environ.get('IS_SHORTLINK', 'False'), False) # <-- Also fixed this
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrinkme.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', '12190fcf5675f8f02ab9a15af2db8cbaf6da4a5f')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.co')
+SHORTLINK_API = environ.get('SHORTLINK_API', '01e87776bcde12c051d1b193cebf75aee6b698b5')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+Ws7VRUlD3CdkNzA1')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = is_enabled(environ.get('IS_TUTORIAL', 'False'), False) # <-- Also fixed this
 
